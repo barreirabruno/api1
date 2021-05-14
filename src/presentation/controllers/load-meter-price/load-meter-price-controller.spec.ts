9e70473782e61147455aa4391c23d27e72a3e4d1
barreirabruno <brunobarreirasilva@gmail.com>
2021-05-14 17:11:34 -0300

    refactor: add MeterPriceStub to load meter price controller test

--- src/presentation/controllers/load-meter-price/load-meter-price-controller.spec.ts
+++ src/presentation/controllers/load-meter-price/load-meter-price-controller.spec.ts
@@ -1,15 +1,27 @@
 import { LoadMeterPriceController } from './load-meter-price-controller'
 import { success } from '../../helpers/httpHelper'
+import { MeterPrice } from '../../../domain/usecases/meter-price'
+import { MeterPriceModel } from '../../../domain/models/meter-price'
+import { mockMeterPrice } from '../../../domain/models/mocks/mock-meter-price'
 
-const meterPriceFromDatabase = {
-  district: 'any_district_name',
-  price: 'any_price'
+const makeMeterPrice = (): MeterPrice => {
+  class MeterPriceStub implements MeterPrice {
+    async load (): Promise<MeterPriceModel> {
+      return await new Promise(resolve => resolve({
+        id: 'any_id',
+        district: 'any_district_name',
+        price: 'any_price'
+      }))
+    }
+  }
+
+  return new MeterPriceStub()
 }
 
 describe('Load meters price controller', () => {
-  test('Should return 200 on success', () => {
-    const sut = new LoadMeterPriceController()
-    const httpResponse = sut.handle({})
-    expect(httpResponse).toEqual(success(meterPriceFromDatabase))
+  test('Should return 200 on success', async () => {
+    const sut = new LoadMeterPriceController(makeMeterPrice())
+    const httpResponse = await sut.handle({})
+    expect(httpResponse).toEqual(success(mockMeterPrice()))
   })
 })
