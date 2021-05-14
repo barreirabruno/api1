bf070611710355116f6af1e8e4762913e42c7c3a
barreirabruno <brunobarreirasilva@gmail.com>
2021-05-14 13:00:17 -0300

    test: load meters price controller should return 200 on success

---
+++ src/presentation/load-meter-price/load-meter-price-controller.spec.ts
@@ -0,0 +1,36 @@
+interface HttpRequest {
+  body?: any
+}
+
+interface HttpResponse {
+  statusCode: number
+  body: any
+}
+
+interface Controller {
+  handle: (httpRequest: HttpRequest) => HttpResponse
+}
+
+const success = (data: any): HttpResponse => ({
+  statusCode: 200,
+  body: data
+})
+
+const meterPriceFromDatabase = {
+  district: 'any_district_name',
+  price: 'any_price'
+}
+
+class LoadMeterPriceController implements Controller {
+  handle (httpRequest: HttpRequest): HttpResponse {
+    return success(meterPriceFromDatabase)
+  }
+}
+
+describe('Load meters price controller', () => {
+  test('Should return 200 on success', () => {
+    const sut = new LoadMeterPriceController()
+    const httpResponse = sut.handle({})
+    expect(httpResponse).toEqual(success(meterPriceFromDatabase))
+  })
+})
