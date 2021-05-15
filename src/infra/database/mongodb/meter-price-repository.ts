50a7130a622a8066c8bbc240ff48affe3a9e00d9
barreirabruno <brunobarreirasilva@gmail.com>
2021-05-14 22:05:53 -0300

    feat: add meter price repository to load a meter price

---
+++ src/infra/database/mongodb/meter-price-repository.ts
@@ -0,0 +1,11 @@
+import { LoadMeterPriceRepository } from '../../../data/protocols/meter-price/load-meter-price-repository'
+import { MeterPriceModel } from '../../../domain/models/meter-price'
+import { MongoHelper } from './helpers/mongo-helper'
+
+export class MeterPriceMongoRepository implements LoadMeterPriceRepository {
+  async loadMeterPrice (): Promise<MeterPriceModel> {
+    const meterPriceCollection = MongoHelper.getCollection('meter-prices')
+    const meterPrice = await meterPriceCollection.findOne({ district: 'any_location' })
+    return MongoHelper.map(meterPrice)
+  }
+}
