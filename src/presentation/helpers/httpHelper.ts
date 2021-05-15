3045b05a3314ce29805168fa7dd5ae73db9ce9cd
barreirabruno <brunobarreirasilva@gmail.com>
2021-05-15 18:43:44 -0300

    refactor: add server error to http helpers

--- src/presentation/helpers/httpHelper.ts
+++ src/presentation/helpers/httpHelper.ts
@@ -1,6 +1,12 @@
 import { HttpResponse } from '../protocols/http'
+import { ServerError } from '../errors/ServerError'
 
 export const success = (data: any): HttpResponse => ({
   statusCode: 200,
   body: data
+})
+
+export const serverError = (error: Error): HttpResponse => ({
+  statusCode: 500,
+  body: new ServerError(error.stack as string)
 })
