7e869985ce49964de1ae9d19cdcc7b30a7adc427
barreirabruno <brunobarreirasilva@gmail.com>
2021-05-15 18:44:13 -0300

    refactor: add server error to custom errors

---
+++ src/presentation/errors/ServerError.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/space-before-function-paren */
+export class ServerError extends Error {
+  constructor(stack: string) {
+    super('Internal Server Error')
+    this.name = 'Server Error'
+    this.stack = stack
+  }
+}
