e0ed3df72f991f6b54365b0f4a6bbe98a316121f
barreirabruno <brunobarreirasilva@gmail.com>
2021-05-15 13:41:26 -0300

    refactor: connect to mongodb before integration tests

--- src/main/routes/meter-price-routes.test.ts
+++ src/main/routes/meter-price-routes.test.ts
@@ -1,7 +1,17 @@
 import request from 'supertest'
 import app from '../config/app'
+import { MongoHelper } from '../../infra/database/mongodb/helpers/mongo-helper'
 
 describe('Meter price route', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+    await MongoHelper.addMeterPrice()
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
   test('Should return a meter price on success', async () => {
     await request(app)
       .get('/api/meter-price')
