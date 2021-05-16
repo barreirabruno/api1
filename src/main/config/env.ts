dd5c254924f7e0ad0d068f3b4e0086ce997e38a0
Bruno Barreira <brunobarreirasilva@gmail.com>
2021-05-16 15:52:56 -0300

    refactor: fix error digit and remove eslint obslolete rule

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/strict-boolean-expressions */
 export default {
   port: process.env.PORT ?? 3333,
   mongoUrl: process.env.MONGO_URL ?? 'mongodb://localhost:27017/in-gaia-mongodb'
