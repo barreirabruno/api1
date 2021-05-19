142c357d0e22720abd5ba9f2fae951854ec1d368
barreirabruno <brunobarreirasilva@gmail.com>
2021-05-19 11:23:19 -0300

    refactor: add dev database running on mongo atlas to api1

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,4 @@
 export default {
   port: process.env.PORT ?? 3333,
-  mongoUrl: process.env.MONGO_URL ?? 'mongodb://mongo:27017/in-gaia-mongodb'
+  mongoUrl: process.env.MONGO_URL ?? 'mongodb+srv://heroku_user:VZM7AssdvmVqdK9m@cluster0.i3cyn.mongodb.net/inGaiaTest?retryWrites=true&w=majority'
 }
